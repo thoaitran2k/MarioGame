b05860482ce197ea19c813f0f9be9d92abc3adf4
thoaitran2k <185214555@gm.uit.edu.vn>
2023-11-20 09:55:35 +0700

    add background

--- 05-SceneManager/PlayScene.cpp
+++ 05-SceneManager/PlayScene.cpp
@@ -12,6 +12,7 @@
 #include "BrickQuestion.h"
 #include "MushRoom.h"
 #include "Map.h"
+#include "Background.h"
 
 
 
@@ -119,7 +120,7 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 
 	switch (object_type)
 	{
-	case OBJECT_TYPE_MAP: obj = new CMap(x, y); break;
+	
 	case OBJECT_TYPE_MARIO:
 		if (player!=NULL) 
 		{
@@ -135,6 +136,7 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 	case OBJECT_TYPE_BRICK: obj = new CBrick(x,y); break;
 	case OBJECT_TYPE_COIN: obj = new CCoin(x, y); break;
 	case OBJECT_TYPE_BRICK_Q: obj = new CBrickQuestion(x, y); break;
+	//case OBJECT_TYPE_MAP: obj = new CMap(x, y); break;
 	
 
 
@@ -150,6 +152,25 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 		int sprite_end = atoi(tokens[8].c_str());
 
 		obj = new CPlatform(
+			x, y,
+			cell_width, cell_height, length,
+			sprite_begin, sprite_middle, sprite_end
+		);
+
+		break;
+	}
+
+	case OBJECT_TYPE_PLATFORM1:
+	{
+
+		float cell_width = (float)atof(tokens[3].c_str());
+		float cell_height = (float)atof(tokens[4].c_str());
+		int length = atoi(tokens[5].c_str());
+		int sprite_begin = atoi(tokens[6].c_str());
+		int sprite_middle = atoi(tokens[7].c_str());
+		int sprite_end = atoi(tokens[8].c_str());
+
+		obj = new CBackground(
 			x, y,
 			cell_width, cell_height, length,
 			sprite_begin, sprite_middle, sprite_end
